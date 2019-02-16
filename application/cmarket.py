23bbc2c1c703d5e9fce9c96169b683a294580db0
farkhodsadykov <sadykovfarkhod@gmail.com>
2019-02-16 16:02:50 -0600

    feature/welcome Created welcome page

---
+++ application/cmarket.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+
+# Create the application.
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    """ Displays the index page accessible at '/'
+    """
+    return render_template('welcome.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000, host='0.0.0.0')
